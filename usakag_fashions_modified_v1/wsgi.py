58bde5a494d99e9cdf5f7d27bbd87805cab8929f
kagolo <84382333+kagolo@users.noreply.github.com>
2022-09-21 14:38:59 -0700

    made changes in wsgi.py

--- usakag_fashions_modified_v1/wsgi.py
+++ usakag_fashions_modified_v1/wsgi.py
@@ -7,15 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os, sys
-# add the usakag_fashions_modified_v1 path into the sys.path
-sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/usakag_fashions_modified_v1')
-
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
-
-# poiting to the project settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usakag_fashions_modified_v1.settings')
+import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usakag_fashions_modified_v1.settings')
+
 application = get_wsgi_application()
