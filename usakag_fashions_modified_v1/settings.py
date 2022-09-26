d2515711df613b829178892901780b740acfa5e2
kagolo <84382333+kagolo@users.noreply.github.com>
2022-09-25 21:41:53 -0700

    changed host to USAKENTS.COM

--- usakag_fashions_modified_v1/settings.py
+++ usakag_fashions_modified_v1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6cqyd1#h0u+%zw9tqiggok2!f%c6im)otyu+nytvhl1oir7(z0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['USAKENTS.COM']
 
 
 # Application definition
